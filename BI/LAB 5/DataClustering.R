9de63cf310c6ac00ed1376d909dadb42dd6aa147
Sarveshkumar Jaiswal <59839006+sarvesh-jaiswal-official@users.noreply.github.com>
2023-05-18 22:36:16 +0530

    Add files via upload

---
+++ BI/LAB 5/DataClustering.R
@@ -0,0 +1,17 @@
+# Get the data points in form of a R vector.
+rainfall <- c(799,1174.8,865.1,1334.6,635.4,918.5,685.5,998.6,784.2,985,882.8,1071)
+
+# Convert it to a time series object.
+rainfall.timeseries <- ts(rainfall,start = c(2012,1),frequency = 12)
+
+# Print the timeseries data.
+print(rainfall.timeseries)
+
+# Give the chart file a name.
+jpeg(file = "rainfall.jpg")
+
+# Plot a graph of the time series.
+plot(rainfall.timeseries)
+
+# Close the graphics device.
+graphics.off()
